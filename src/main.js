429ed1411e900f76ca2610b511603c743bf729bb
Chris Thomas <chris.thomas@antimatter-studios.com>
2022-02-03 18:04:42 +0100

    add the SSM and cognito endpoints

--- src/main.js
+++ src/main.js
@@ -5,7 +5,21 @@ const AWS = require("aws-sdk");
 class ServerlessAwsConfigPlugin {
     constructor() {
         if(process.env.AWS_ENDPOINT_SSM){
-            AWS.config.ssm = { endpoint: process.env.AWS_ENDPOINT_SSM };
+            AWS.config.ssm = { 
+                endpoint: process.env.AWS_ENDPOINT_SSM 
+            };
+        }
+        
+        if(process.env.AWS_ENDPOINT_COGNITO_IDENTITY){
+            AWS.config.cognitoidentity = { 
+                endpoint: process.env.AWS_ENDPOINT_COGNITO_IDENTITY 
+            };
+        }
+
+        if(process.env.AWS_ENDPOINT_COGNITO_IDENTITY_SERVICE_PROVIDER){
+            AWS.config.cognitoidentityserviceprovider = { 
+                endpoint: process.env.AWS_ENDPOINT_COGNITO_IDENTITY_SERVICE_PROVIDER 
+            };
         }
     }
 }
